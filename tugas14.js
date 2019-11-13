ac2c2d71f2115a76e4113520fb4d21588f195855
Havid <great_havid@yahoo.co.id>
2019-11-13 11:41:51 +0700

    Tugas 14 JS

---
+++ tugas14.js
@@ -0,0 +1,11 @@
+// ====== Tugas 14 JavaScript =====
+
+var x = 37;
+var date = 6;
+
+x_date = x / date;
+console.log(x_date)
+
+console.log(Math.ceil(x_date));
+console.log(Math.floor(x_date));
+console.log(Math.round(x_date));
